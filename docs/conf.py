e5ab6bd5f89de793e638b070fc9e8b66bbec3897
mjiang102628 <mjiang102628@gmail.com>
2020-02-26 14:40:46 -0500

    don't sort entries alphabetically

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('..'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -63,6 +63,7 @@ html_theme_options = {
     'display_version': False}
 
 add_module_names = False
+autodoc_member_order = 'bysource'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
