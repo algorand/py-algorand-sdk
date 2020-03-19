9c9e52a7349e22808da77992867765e0cbf65fd4
mjiang102628 <mjiang102628@gmail.com>
2020-03-19 14:13:42 -0400

    Update run_docker.sh

--- test/cucumber/docker/run_docker.sh
+++ test/cucumber/docker/run_docker.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf temp
 rm -rf test/cucumber/features
-git clone --single-branch --branch michelle/test https://github.com/algorand/algorand-sdk-testing.git temp
+git clone --single-branch --branch templates https://github.com/algorand/algorand-sdk-testing.git temp
 
 cp test/cucumber/docker/sdk.py temp/docker
 mv temp/features test/cucumber/features
