c9c9d44fe77744d4e236a91d4022a9f71cec4ced
Will Winder <wwinder.unh@gmail.com>
2020-05-06 06:43:07 -0400

    Update python to use new test environment.

--- test/cucumber/docker/run_docker.sh
+++ test/cucumber/docker/run_docker.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
-
 set -e
+
+# Move to project root, relative to script location
+rootdir=`dirname $0`
+pushd "${rootdir}/../../.."
 
 rm -rf temp
 rm -rf test/cucumber/features
-git clone --single-branch --branch templates https://github.com/algorand/algorand-sdk-testing.git temp
+git clone --single-branch --branch develop https://github.com/algorand/algorand-sdk-testing.git temp
 
-cp test/cucumber/docker/sdk.py temp/docker
 mv temp/features test/cucumber/features
 
-docker build -t sdk-testing -f test/cucumber/docker/Dockerfile "$(pwd)"
+docker build -t py-sdk-testing -f test/cucumber/docker/Dockerfile "$(pwd)"
 
+# Start test harness environment
+./temp/scripts/up.sh
+
+# Build and launch SDK testing container
 docker run -it \
-     -v "$(pwd)":/opt/py-algorand-sdk \
-     sdk-testing:latest 
+     --network host \
+     py-sdk-testing:latest 
