1ea8270536fe0dde39f7a25ac48cd66be3f397d6
mjiang102628 <mjiang102628@gmail.com>
2020-10-25 17:52:09 -0400

    add wait_for_confirmation function

--- algosdk/v2client/algod.py
+++ algosdk/v2client/algod.py
@@ -188,6 +188,31 @@ class AlgodClient:
         headers = { 'Content-Type': 'application/x-binary' }
         return self.algod_request("POST", req, data=txn, headers=headers, **kwargs)["txId"]
 
+    def wait_for_confirmation(self, transaction_id, timeout):
+        """
+        Wait until the transaction is confirmed or rejected, or until 'timeout'
+        number of rounds have passed.
+
+        Args:
+            transaction_id (str): the transaction to wait for
+            timeout (int): maximum number of rounds to wait
+        
+        Returns:
+            dict: pending transaction information, or None if the transaction
+                is not confirmed or rejected in the next timeout rounds
+        """
+        start_round = self.status()["next-version-round"]
+        current_round = start_round
+
+        while current_round < start_round + timeout:
+            self.status_after_block(current_round)
+            pending_txn = self.pending_transaction_info(transaction_id)
+            if pending_txn.get("confirmed-round", 0) > 0:
+                return pending_txn
+            elif pending_txn["pool-error"]:
+                return pending_txn
+            current_round += 1
+
     def pending_transactions(self, max_txns=0, response_format="json", **kwargs):
         """
         Return pending transactions.
