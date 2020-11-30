d48a7cf491da8d899489d4bf1b356955fe92b119
John Jannotti <jj@cs.brown.edu>
2020-11-30 10:29:19 -0500

    Be forgiving about typing of app_args, foreign_a*
    Allow callers to specify args as strings, not just bytes, and
    foreign_apps and assets as strings instead of just ints.  This makes
    it more convenient to pull these things from environment variables.

--- algosdk/future/transaction.py
+++ algosdk/future/transaction.py
@@ -959,15 +959,35 @@ class ApplicationCallTxn(Transaction):
         self.global_schema = global_schema
         self.approval_program = approval_program
         self.clear_program = clear_program
-        self.app_args = app_args
+        self.app_args = self.bytes_list(app_args)
         self.accounts = accounts
-        self.foreign_apps = foreign_apps
-        self.foreign_assets = foreign_assets
+        self.foreign_apps = self.int_list(foreign_apps)
+        self.foreign_assets = self.int_list(foreign_assets)
         if sp.flat_fee:
             self.fee = max(constants.min_txn_fee, self.fee)
         else:
             self.fee = max(self.estimate_size() * self.fee,
                            constants.min_txn_fee)
+
+
+    @staticmethod
+    def bytes_list(lst):
+        def as_bytes(e):
+            if isinstance(e, bytes):
+                return e
+            if isinstance(e, str):
+                return e.encode()
+            assert False, f"{e} is neither bytes nor str"
+
+        if not lst:
+            return lst
+        return [as_bytes(elt) for elt in lst]
+
+    @staticmethod
+    def int_list(lst):
+        if not lst:
+            return lst
+        return [int(elt) for elt in lst]
 
     def dictify(self):
         d = dict()
