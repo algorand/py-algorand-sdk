b7fb15ceec1c2f5b2af362fbf3482f78e6919422
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-03-31 11:15:07 -0400

    merge fmt

--- test_unit.py
+++ test_unit.py
@@ -3698,7 +3698,7 @@ class TestABIEncoding(unittest.TestCase):
                 expected = val
                 self.assertEqual(actual, expected)
             # Test for the upper limit of each bit size
-            val = 2**uint_size - 1
+            val = 2 ** uint_size - 1
             uint_type = UintType(uint_size)
             actual = uint_type.encode(val)
             self.assertEqual(len(actual), uint_type.bit_size // 8)
@@ -3714,7 +3714,7 @@ class TestABIEncoding(unittest.TestCase):
             with self.assertRaises(error.ABIEncodingError) as e:
                 UintType(uint_size).encode(-1)
             with self.assertRaises(error.ABIEncodingError) as e:
-                UintType(uint_size).encode(2**uint_size)
+                UintType(uint_size).encode(2 ** uint_size)
             with self.assertRaises(error.ABIEncodingError) as e:
                 UintType(uint_size).decode("ZZZZ")
             with self.assertRaises(error.ABIEncodingError) as e:
@@ -3737,7 +3737,7 @@ class TestABIEncoding(unittest.TestCase):
                     expected = val
                     self.assertEqual(actual, expected)
             # Test for the upper limit of each bit size
-            val = 2**ufixed_size - 1
+            val = 2 ** ufixed_size - 1
             ufixed_type = UfixedType(ufixed_size, precision)
             actual = ufixed_type.encode(val)
             self.assertEqual(len(actual), ufixed_type.bit_size // 8)
@@ -3753,7 +3753,7 @@ class TestABIEncoding(unittest.TestCase):
             with self.assertRaises(error.ABIEncodingError) as e:
                 UfixedType(ufixed_size, 10).encode(-1)
             with self.assertRaises(error.ABIEncodingError) as e:
-                UfixedType(ufixed_size, 10).encode(2**ufixed_size)
+                UfixedType(ufixed_size, 10).encode(2 ** ufixed_size)
             with self.assertRaises(error.ABIEncodingError) as e:
                 UfixedType(ufixed_size, 10).decode("ZZZZ")
             with self.assertRaises(error.ABIEncodingError) as e:
