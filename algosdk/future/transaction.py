102874957fbc6f01855c088267a1a2ca320e5c0c
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-04-26 12:48:12 -0400

    adding foreign-app-addr to dryrun creator (#321)

--- algosdk/future/transaction.py
+++ algosdk/future/transaction.py
@@ -3131,6 +3131,12 @@ def create_dryrun(
                 accts.extend(txn.accounts)
             if txn.foreign_apps:
                 apps.extend(txn.foreign_apps)
+                accts.extend(
+                    [
+                        logic.get_application_address(aidx)
+                        for aidx in txn.foreign_apps
+                    ]
+                )
             if txn.foreign_assets:
                 assets.extend(txn.foreign_assets)
 
