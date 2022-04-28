6622d8106b72a9b95c587dc53eabb62e360958fa
Michael Diamant <michaeldiamant@users.noreply.github.com>
2022-04-28 13:24:11 -0400

    Bump minimum Python version to 3.8 (#323)

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "algosdk"
-copyright = "2020 Algorand"
+copyright = "2022 Algorand"
 author = "Algorand"
 
 
