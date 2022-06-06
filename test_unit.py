959ec1d58f7447794ed26d3c3249f7d4122c0240
algochoi <86622919+algochoi@users.noreply.github.com>
2022-06-06 14:44:37 -0400

    Change unit test to pass in None foreign array

--- test_unit.py
+++ test_unit.py
@@ -4237,7 +4237,7 @@ class TestBoxReference(unittest.TestCase):
                 error.InvalidForeignIndexError,
             ),
             ([("tomato", "tomato")], [1], ValueError),
-            ([(2, "zomato")], [], error.InvalidForeignIndexError),
+            ([(2, "zomato")], None, error.InvalidForeignIndexError),
         ]
 
         for test_case in test_cases_id_error:
