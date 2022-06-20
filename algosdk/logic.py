c03a53d8d333787494c44f339ede4a97e6f67acf
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-06-20 10:29:01 -0400

    adding src map decoder

--- algosdk/logic.py
+++ algosdk/logic.py
@@ -2,6 +2,8 @@ import base64
 import json
 import os
 
+from typing import Tuple, Dict, List, Any
+
 from . import constants
 from . import error
 from . import encoding
@@ -287,3 +289,60 @@ def get_application_address(appID: int) -> str:
     to_sign = constants.APPID_PREFIX + appID.to_bytes(8, "big")
     checksum = encoding.checksum(to_sign)
     return encoding.encode_address(checksum)
+
+
+def decode_source_map(src_map: Dict[str, Any], delimiter: str = ";"):
+    raw_mapping = src_map["mapping"].split(delimiter)
+    pc_list = [_decode_int_value(raw_val) for raw_val in raw_mapping]
+
+    line_map = {}
+    for index, line_num in enumerate(pc_list):
+        if line_num is not None:  # be careful for '0' checks!
+            if line_num not in line_map:
+                line_map[line_num] = []
+            line_map[line_num].append(index)
+    return line_map
+
+def _decode_int_value(value: str) -> int:
+    decoded_value = base64vlq_decode(value)
+    return decoded_value[2] if decoded_value else None
+
+_b64chars = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+_b64table = [None] * (max(_b64chars) + 1)
+for i, b in enumerate(_b64chars):
+    _b64table[b] = i
+
+_shiftsize, _flag, _mask = 5, 1 << 5, (1 << 5) - 1
+
+def base64vlq_decode(vlqval: str) -> Tuple[int]:
+    """Decode Base64 VLQ value"""
+    results = []
+    add = results.append
+    shiftsize, flag, mask = _shiftsize, _flag, _mask
+    shift = value = 0
+    # use byte values and a table to go from base64 characters to integers
+    for v in map(_b64table.__getitem__, vlqval.encode("ascii")):
+        value += (v & mask) << shift
+        if v & flag:
+            shift += shiftsize
+            continue
+        # determine sign and add to results
+        add((value >> 1) * (-1 if value & 1 else 1))
+        shift = value = 0
+    return results
+
+
+def base64vlq_encode(*values: int) -> str:
+    """Encode integers to a VLQ value"""
+    results = []
+    add = results.append
+    shiftsize, flag, mask = _shiftsize, _flag, _mask
+    for v in values:
+        # add sign bit
+        v = (abs(v) << 1) | int(v < 0)
+        while True:
+            toencode, v = v & mask, v >> shiftsize
+            add(toencode | (v and flag))
+            if not v:
+                break
+    return bytes(map(_b64chars.__getitem__, results)).decode()
