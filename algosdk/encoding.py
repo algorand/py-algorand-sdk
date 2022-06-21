2b82f296eb6be63dc03eb3215696608fd4ee6acc
algochoi <86622919+algochoi@users.noreply.github.com>
2022-06-21 12:41:01 -0400

    Finish merging

--- algosdk/encoding.py
+++ algosdk/encoding.py
@@ -1,9 +1,6 @@
 import base64
 from collections import OrderedDict
-<<<<<<< HEAD
-=======
 from typing import Union
->>>>>>> feature/box-storage
 
 import msgpack
 from Cryptodome.Hash import SHA512
@@ -243,13 +240,9 @@ def checksum(data):
     return chksum.digest()
 
 
-<<<<<<< HEAD
-def encode_as_bytes(e):
-=======
 def encode_as_bytes(
     e: Union[bytes, bytearray, str, int]
 ) -> Union[bytes, bytearray]:
->>>>>>> feature/box-storage
     """Confirm or coerce element to bytes."""
     if isinstance(e, (bytes, bytearray)):
         return e
