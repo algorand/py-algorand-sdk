d951d7530a0bdd580b0983a654a83df5df370a63
algochoi <86622919+algochoi@users.noreply.github.com>
2022-07-08 01:26:00 -0400

    Revise and delete v1 algod steps and add devmode helpers

--- tests/steps/other_v2_steps.py
+++ tests/steps/other_v2_steps.py
@@ -107,7 +107,7 @@ def send_zero_transactions(context, txns=1):
             sp,
             constants.ZERO_ADDRESS,
             0,
-            note=random.randbytes(8)
+            note=random.randbytes(8),
         )
         signed_payment = context.wallet.sign_transaction(payment)
         context.app_acl.send_transaction(signed_payment)
