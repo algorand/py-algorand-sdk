2af8a710be9d57bd6c86f001812a4033dd033de0
Zeph Grunschlag <zeph@algorand.com>
2022-08-03 18:29:57 -0500

    time printout

--- run_integration.sh
+++ run_integration.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+START=$(date "+%s")
+
 set -e
 
 rootdir=`dirname $0`
@@ -7,24 +9,32 @@ pushd $rootdir
 
 # Reset test harness
 rm -rf test-harness
-git clone --single-branch --branch master https://github.com/algorand/algorand-sdk-testing.git test-harness
+git clone --single-branch --branch trim-indexer https://github.com/algorand/algorand-sdk-testing.git test-harness
 
 ## Copy feature files into the project resources
 mkdir -p tests/features
 cp -r test-harness/features/* tests/features
+echo "time till end of cloning + copying: " + $(($(date "+%s") - $START))
+
 
 # Build SDK testing environment
 docker build -t py-sdk-testing --build-arg PYTHON_VERSION="${PYTHON_VERSION}" -f Dockerfile "$(pwd)"
+echo "time till end of building PYSDK: " + $(($(date "+%s") - $START))
+
 
 # Start test harness environment
 ./test-harness/scripts/up.sh
+echo "time till end of up.sh: " + $(($(date "+%s") - $START))
 
-while [ $(curl -sL -w "%{http_code}\\n" "http://localhost:59999/v2/accounts" -o /dev/null --connect-timeout 3 --max-time 5) -ne "200" ]
+while [ $(curl -sL -w "%{http_code}\\n" "http://localhost:60002/v2/accounts" -o /dev/null --connect-timeout 3 --max-time 5) -ne "200" ]
 do
   sleep 1
 done
+echo "time till indexer-live is ready: " + $(($(date "+%s") - $START))
 
 # Launch SDK testing
 docker run -it \
      --network host \
      py-sdk-testing:latest 
+echo "time till end of all tests: " + $(($(date "+%s") - $START))
+
