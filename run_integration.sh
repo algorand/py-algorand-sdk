4fc4137456c1fb4341b2f69bd71c4d508f75ba29
Zeph Grunschlag <zeph@algorand.com>
2022-08-05 09:46:22 -0500

    bootstrap till the point of running tests

--- run_integration.sh
+++ run_integration.sh
@@ -4,31 +4,45 @@ START=$(date "+%s")
 
 set -e
 
-rootdir=`dirname $0`
-pushd $rootdir
+ENV_FILE=".test-env"
+source $ENV_FILE
+
+rootdir=$(dirname "$0")
+pushd "$rootdir"
 
 # Reset test harness
-rm -rf test-harness
-git clone --single-branch --branch trim-indexer https://github.com/algorand/algorand-sdk-testing.git test-harness
+rm -rf "$SDK_TESTING_HARNESS"
+git clone --single-branch --branch "$SDK_TESTING_BRANCH" "$SDK_TESTING_URL" "$SDK_TESTING_HARNESS"
+
+# OVERWRITE incoming .env with .test-env
+cp "$ENV_FILE" "$SDK_TESTING_HARNESS"/.env
 
 ## Copy feature files into the project resources
 mkdir -p tests/features
-cp -r test-harness/features/* tests/features
-echo "time till end of cloning + copying: " + $(($(date "+%s") - $START))
-
-
-# Build SDK testing environment
-docker build -t py-sdk-testing --build-arg PYTHON_VERSION="${PYTHON_VERSION}" -f Dockerfile "$(pwd)"
-echo "time till end of building PYSDK: " + $(($(date "+%s") - $START))
+cp -r "$SDK_TESTING_HARNESS"/features/* tests/features
+echo "run_integration.sh: seconds it took to get to end of cloning + copying: " + $(($(date "+%s") - $START))
 
 
 # Start test harness environment
-./test-harness/scripts/up.sh
-echo "time till end of up.sh: " + $(($(date "+%s") - $START))
+pushd "$SDK_TESTING_HARNESS"
+./scripts/up.sh
+popd
+echo "run_integration.sh: seconds it took to finish testing sdk's up.sh: " + $(($(date "+%s") - $START))
+echo "To run sandbox commands, cd into $SDK_TESTING_HARNESS/$SANDBOX"
 
-# Launch SDK testing
-docker run -it \
-     --network host \
-     py-sdk-testing:latest 
+if [[ $SKIP_PYTHON_SDK_DOCKER == 1 ]]; then
+     echo "SKIP dockerizing the python sdk."
+     echo "Ready for manual testing."
+else
+     # Build Docker for python environment
+     echo "dockerizing the python sdk"
+     docker build -t py-sdk-testing --build-arg PYTHON_VERSION="${PYTHON_VERSION}" -f Dockerfile "$(pwd)"
+     echo "run_integration.sh: seconds it took to finish building PYSDK: " + $(($(date "+%s") - $START))
+     # Launch SDK testing
+     docker run -it \
+          --network host \
+          py-sdk-testing:latest 
+fi
+
 echo "time till end of all tests: " + $(($(date "+%s") - $START))
 
