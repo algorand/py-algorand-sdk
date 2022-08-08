1341f90937daff1dbca8b3fc24d14ad7573c300b
Zeph Grunschlag <zeph@algorand.com>
2022-08-07 23:42:28 -0500

    stale log

--- test-harness.sh
+++ test-harness.sh
@@ -28,13 +28,13 @@ cp "$ENV_FILE" "$SDK_TESTING_HARNESS"/.env
 ## Copy feature files into the project resources
 mkdir -p tests/features
 cp -r "$SDK_TESTING_HARNESS"/features/* tests/features
-echo "run_integration.sh: seconds it took to get to end of cloning + copying: " + $(($(date "+%s") - $START))
+echo "test-harness.sh: seconds it took to get to end of cloning + copying: " + $(($(date "+%s") - $START))
 
 
 ## Start test harness environment
 pushd "$SDK_TESTING_HARNESS"
 ./scripts/up.sh
 popd
-echo "run_integration.sh: seconds it took to finish testing sdk's up.sh: " + $(($(date "+%s") - $START))
+echo "test-harness.sh: seconds it took to finish testing sdk's up.sh: " + $(($(date "+%s") - $START))
 echo "To run sandbox commands, cd into $SDK_TESTING_HARNESS/$SANDBOX"
 echo "time till end of test-harness bootstrapping: " + $(($(date "+%s") - $START))
