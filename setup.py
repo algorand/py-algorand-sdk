6003b37e284ca5ea043cd5697e46dc7475c5084a
algochoi <86622919+algochoi@users.noreply.github.com>
2022-12-23 12:58:15 -0500

    Fix warnings on version in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="Algorand SDK in Python",
     author="Algorand",
     author_email="pypiservice@algorand.com",
-    version="v1.20.2",
+    version="1.20.2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/algorand/py-algorand-sdk",
