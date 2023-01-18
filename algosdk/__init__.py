802380f3cc414e6fe56a5dccf65860547382cae7
Zeph Grunschlag <tzaffi@users.noreply.github.com>
2023-01-18 08:47:24 -0600

    Enhancement: Type Friendly Exports (#435)
    * Type Friendly Exports

--- algosdk/__init__.py
+++ algosdk/__init__.py
@@ -1,18 +1,48 @@
-from . import abi
-from . import account
-from . import auction
-from . import constants
-from . import dryrun_results
-from . import encoding
-from . import error
-from . import kmd
-from . import logic
-from . import mnemonic
-from . import transaction
-from . import util
-from . import v2client
-from . import wallet
-from . import wordlist
-from . import source_map
+from . import (
+    abi,
+    account,
+    auction,
+    constants,
+    dryrun_results,
+    encoding,
+    error,
+    kmd,
+    logic,
+    mnemonic,
+    source_map,
+    transaction,
+    util,
+    v2client,
+    wallet,
+    wordlist,
+)
+
+from .abi import __all__ as abi_all
+from .v2client import __all__ as v2client_all
+
+# begin __all__
+__all__ = (
+    abi_all
+    + v2client_all
+    + [
+        "abi",
+        "account",
+        "auction",
+        "constants",
+        "dryrun_results",
+        "encoding",
+        "error",
+        "kmd",
+        "logic",
+        "mnemonic",
+        "source_map",
+        "transaction",
+        "util",
+        "v2client",
+        "wallet",
+        "wordlist",
+    ]
+)  # type: ignore
+# end __all__
 
 name = "algosdk"
