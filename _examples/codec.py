74af29ca6b5bce0a9acf9430af7ca1013a4d77b4
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-02-21 12:16:38 -0500

    fmt

--- _examples/codec.py
+++ _examples/codec.py
@@ -1,33 +1,34 @@
-import base64 
+import base64
 import os
 from utils import get_algod_client, get_accounts
 from algosdk import transaction
-from algosdk import encoding 
-# example: CODEC_ADDRESS                
+from algosdk import encoding
+
+# example: CODEC_ADDRESS
 address = "4H5UNRBJ2Q6JENAXQ6HNTGKLKINP4J4VTQBEPK5F3I6RDICMZBPGNH6KD4"
 pk = encoding.decode_address(address)
 addr = encoding.encode_address(pk)
 
 assert addr == address
-# example: CODEC_ADDRESS                
+# example: CODEC_ADDRESS
 
 # example: CODEC_BASE64
 encoded_str = "SGksIEknbSBkZWNvZGVkIGZyb20gYmFzZTY0"
-decoded_str = base64.b64decode(encoded_str).decode('utf-8')
+decoded_str = base64.b64decode(encoded_str).decode("utf-8")
 print(decoded_str)
-# example: CODEC_BASE64                    
+# example: CODEC_BASE64
 
-# example: CODEC_UINT64                 
+# example: CODEC_UINT64
 val = 1337
-encoded_uint = val.to_bytes(8,'big')
-decoded_uint = int.from_bytes(encoded_uint, byteorder='big')
+encoded_uint = val.to_bytes(8, "big")
+decoded_uint = int.from_bytes(encoded_uint, byteorder="big")
 assert decoded_uint == val
-# example: CODEC_UINT64                 
+# example: CODEC_UINT64
 
 
 algod_client = get_algod_client()
 acct = get_accounts().pop()
-# example: CODEC_TRANSACTION_UNSIGNED   
+# example: CODEC_TRANSACTION_UNSIGNED
 sp = algod_client.suggested_params()
 pay_txn = transaction.PaymentTxn(acct.address, sp, acct.address, 10000)
 
@@ -40,11 +41,11 @@ with open("pay.txn", "r") as f:
     recovered_txn = encoding.msgpack_decode(f.read())
 
 print(recovered_txn.dictify())
-# example: CODEC_TRANSACTION_UNSIGNED   
+# example: CODEC_TRANSACTION_UNSIGNED
 
 os.remove("pay.txn")
 
-# example: CODEC_TRANSACTION_SIGNED     
+# example: CODEC_TRANSACTION_SIGNED
 # Sign transaction
 spay_txn = pay_txn.sign(acct.private_key)
 # write message packed signed transaction to disk
@@ -56,11 +57,10 @@ with open("signed_pay.txn", "r") as f:
     recovered_signed_txn = encoding.msgpack_decode(f.read())
 
 print(recovered_signed_txn.dictify())
-# example: CODEC_TRANSACTION_SIGNED     
+# example: CODEC_TRANSACTION_SIGNED
+
 
 os.remove("signed_pay.txn")
 
-# example: CODEC_BLOCK                  
-# example: CODEC_BLOCK                  
-
-
+# example: CODEC_BLOCK
+# example: CODEC_BLOCK
