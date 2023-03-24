d28452a3faa791c0e5b16fdc44a333f7a96bc881
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-03-24 11:13:01 -0400

    missed multisig in txn types

--- algosdk/transaction.py
+++ algosdk/transaction.py
@@ -3233,7 +3233,9 @@ defaultAppId = 1380011588
 
 def create_dryrun(
     client: algod.AlgodClient,
-    txns: List[Union[SignedTransaction, LogicSigTransaction]],
+    txns: List[
+        Union[SignedTransaction, LogicSigTransaction, MultisigTransaction]
+    ],
     protocol_version=None,
     latest_timestamp=None,
     round=None,
