0cfbe676d6f6fdc2e1f862608f07812771e4553b
Jason Paulos <jasonpaulos@users.noreply.github.com>
2023-04-14 10:51:06 -0700

    No primative type indexes in Python 3.8

--- algosdk/v2client/models/simulate_request.py
+++ algosdk/v2client/models/simulate_request.py
@@ -1,30 +1,30 @@
-from typing import TYPE_CHECKING
+from typing import List, Dict, Any, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from algosdk import transaction
 
 
 class SimulateRequestTransactionGroup(object):
-    txns: "list[transaction.GenericSignedTransaction]"
+    txns: "List[transaction.GenericSignedTransaction]"
 
     def __init__(
-        self, *, txns: "list[transaction.GenericSignedTransaction]"
+        self, *, txns: "List[transaction.GenericSignedTransaction]"
     ) -> None:
         self.txns = txns
 
-    def dictify(self) -> dict:
+    def dictify(self) -> Dict[str, Any]:
         return {"txns": [txn.dictify() for txn in self.txns]}
 
 
 class SimulateRequest(object):
-    txn_groups: list[SimulateRequestTransactionGroup]
+    txn_groups: List[SimulateRequestTransactionGroup]
 
     def __init__(
-        self, *, txn_groups: list[SimulateRequestTransactionGroup]
+        self, *, txn_groups: List[SimulateRequestTransactionGroup]
     ) -> None:
         self.txn_groups = txn_groups
 
-    def dictify(self) -> dict:
+    def dictify(self) -> Dict[str, Any]:
         return {
             "txn-groups": [
                 txn_group.dictify() for txn_group in self.txn_groups
