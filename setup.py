4940e780f291e5c2b7b1c20b123b437307b99d7c
Barbara Poon <barbara.poon@algorand.com>
2023-05-05 16:24:31 -0400

    Bumped version to v2.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="Algorand SDK in Python",
     author="Algorand",
     author_email="pypiservice@algorand.com",
-    version="2.1.2",
+    version="2.2.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/algorand/py-algorand-sdk",
