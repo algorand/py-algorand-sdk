8f3a2486a31f126ce39ebaaeb60fc429ba41b0b3
Zeph Grunschlag <tzaffi@users.noreply.github.com>
2023-05-15 18:08:27 -0500

    Enhancement: Adding `box_reference.py` to Read The Docs (#481)
    * Adding `box_reference.py` to Read The Docs + updating copyright year to 2023

---
+++ docs/algosdk/box_reference.rst
@@ -0,0 +1,7 @@
+box_reference
+=============
+
+.. automodule:: algosdk.box_reference
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "algosdk"
-copyright = "2022 Algorand"
+copyright = "2023 Algorand"
 author = "Algorand"
 
 
@@ -75,4 +75,4 @@ html_sidebars = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
