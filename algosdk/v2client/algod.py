3ba95bc8772a9b9127a57dafb689f98e9a87e379
algochoi <86622919+algochoi@users.noreply.github.com>
2023-05-16 15:42:49 -0400

    Add ledger state delta APIs to algod

--- algosdk/v2client/algod.py
+++ algosdk/v2client/algod.py
@@ -718,6 +718,46 @@ class AlgodClient:
         req = f"/devmode/blocks/offset/{offset}"
         return self.algod_request("POST", req, **kwargs)
 
+    def get_ledger_state_delta(
+        self, round: int, **kwargs: Any
+    ) -> AlgodResponseType:
+        """
+        Get the ledger state delta for a round.
+        This feature is only available in follower mode.
+
+        Returns:
+            Dict[str, Any]: Response from algod
+        """
+        req = f"/deltas/{round}"
+        return self.algod_request("GET", req, **kwargs)
+
+    def get_transaction_group_ledger_state_deltas_for_round(
+        self, round: int, **kwargs: Any
+    ) -> AlgodResponseType:
+        """
+        Get the ledger state deltas for a transaction group in a given round.
+        This feature is only available in follower mode.
+
+        Returns:
+            Dict[str, Any]: Response from algod
+        """
+        req = f"/deltas/{round}/txn/group"
+        return self.algod_request("GET", req, **kwargs)
+
+    def get_ledger_state_delta_for_transaction_group(
+        self, id: str, **kwargs: Any
+    ) -> AlgodResponseType:
+        """
+        Get the ledger state delta for a transaction group given the
+        transaction or group ID.
+        This feature is only available in follower mode.
+
+        Returns:
+            Dict[str, Any]: Response from algod
+        """
+        req = f"/deltas/txn/group/{id}"
+        return self.algod_request("GET", req, **kwargs)
+
 
 def _specify_round_string(
     block: Union[int, None], round_num: Union[int, None]
