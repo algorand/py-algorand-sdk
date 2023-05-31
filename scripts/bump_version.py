a1a99fe4d44a4b413b2cdb04a0e68053140cc7cb
Barbara Poon <barbara.poon@algorand.com>
2023-05-31 11:25:35 -0400

    move bump_version.py and uncomment create pull request step

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -3,16 +3,17 @@ import re
 
 parser = argparse.ArgumentParser(description="updates the version for a release",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--new_version", help="New Version")
-# parser.add_argument("-b", "--bump", help="version section to bump")
+parser.add_argument("new_version", help="New Version as major.minor.patch")
+parser.add_argument("-s", "--setup_py_path", default="setup.py", help="path to setup.py")
 
 args = parser.parse_args()
 new_version = args.new_version
+setup_py_path = args.setup_py_path
 
-with open("setup.py", "r") as file:
+with open(setup_py_path, "r") as file:
     setup_py = file.read()
 
 new_setup_py = re.sub('version="[0-9]+\.[0-9]+\.[0-9]+"',f"version=\"{new_version}\"", setup_py)
 
-with open("setup.py", "w") as file:
+with open(setup_py_path, "w") as file:
     file.write(new_setup_py)
