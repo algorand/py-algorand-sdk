dc6eae19b4735275d24fea7a0f066e81d5367fd3
Barbara Poon <barbara.poon@algorand.com>
2023-06-01 09:18:48 -0400

    fix lint

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -1,10 +1,14 @@
 import argparse
 import re
 
-parser = argparse.ArgumentParser(description="updates the version for a release",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(
+    description="updates the version for a release",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
 parser.add_argument("new_version", help="New Version as major.minor.patch")
-parser.add_argument("-s", "--setup_py_path", default="setup.py", help="path to setup.py")
+parser.add_argument(
+    "-s", "--setup_py_path", default="setup.py", help="path to setup.py"
+)
 
 args = parser.parse_args()
 new_version = args.new_version
@@ -13,7 +17,9 @@ setup_py_path = args.setup_py_path
 with open(setup_py_path, "r") as file:
     setup_py = file.read()
 
-new_setup_py = re.sub('version="[0-9]+\.[0-9]+\.[0-9]+"',f"version=\"{new_version}\"", setup_py)
+new_setup_py = re.sub(
+    'version="[0-9]+\.[0-9]+\.[0-9]+"', f'version="{new_version}"', setup_py
+)
 
 with open(setup_py_path, "w") as file:
     file.write(new_setup_py)
