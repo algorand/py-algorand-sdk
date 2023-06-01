fc3f14caefece1a1950e986f7496f526550b7178
Barbara Poon <barbara.poon@algorand.com>
2023-06-01 13:13:05 -0400

    follow conventions for bump_version.py

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -4,25 +4,29 @@
 import argparse
 import re
 
-parser = argparse.ArgumentParser(
-    description="updates the version for a release",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument("new_version", help="New Version as major.minor.patch")
-parser.add_argument(
-    "-s", "--setup_py_path", default="setup.py", help="path to setup.py"
-)
+def bump_version(new_version, setup_py_path):
+    with open(setup_py_path, "r") as file:
+        setup_py = file.read()
 
-args = parser.parse_args()
-new_version = args.new_version
-setup_py_path = args.setup_py_path
+    new_setup_py = re.sub(
+        'version="[0-9]+\.[0-9]+\.[0-9]+"', f'version="{new_version}"', setup_py
+    )
 
-with open(setup_py_path, "r") as file:
-    setup_py = file.read()
+    with open(setup_py_path, "w") as file:
+        file.write(new_setup_py)
 
-new_setup_py = re.sub(
-    'version="[0-9]+\.[0-9]+\.[0-9]+"', f'version="{new_version}"', setup_py
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="updates the version for a release",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "new_version", help="New Version as major.minor.patch"
+    )
+    parser.add_argument(
+        "-s", "--setup_py_path", default="setup.py", help="path to setup.py"
+    )
 
-with open(setup_py_path, "w") as file:
-    file.write(new_setup_py)
+    args = parser.parse_args()
+
+    bump_version(args.new_version, args.setup_py_path)
