a14e5303aaa3ab28220cb016c4510458881a7a7f
Barbara Poon <barbara.poon@algorand.com>
2023-06-01 13:19:13 -0400

    reformat bump_version.py

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -4,25 +4,27 @@
 import argparse
 import re
 
+
 def bump_version(new_version, setup_py_path):
     with open(setup_py_path, "r") as file:
         setup_py = file.read()
 
     new_setup_py = re.sub(
-        'version="[0-9]+\.[0-9]+\.[0-9]+"', f'version="{new_version}"', setup_py
+        'version="[0-9]+\.[0-9]+\.[0-9]+"',
+        f'version="{new_version}"',
+        setup_py,
     )
 
     with open(setup_py_path, "w") as file:
         file.write(new_setup_py)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="updates the version for a release",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument(
-        "new_version", help="New Version as major.minor.patch"
-    )
+    parser.add_argument("new_version", help="New Version as major.minor.patch")
     parser.add_argument(
         "-s", "--setup_py_path", default="setup.py", help="path to setup.py"
     )
