73d02b69ab5427a542f605f737ff0f2c9b5632d6
Will Winder <wwinder.unh@gmail.com>
2023-09-19 14:42:28 -0400

    Reformat with black.

--- tests/steps/other_v2_steps.py
+++ tests/steps/other_v2_steps.py
@@ -1694,6 +1694,7 @@ def get_ledger_state_delta_for_transaction_group(context, id):
         )
     )
 
+
 @when("we make a GetBlockTxids call against block number {round}")
 def get_block_txids_call(context, round):
     context.response = context.acl.get_block_txids(round)
